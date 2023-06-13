9d517abbe086a9c4da8fbba4f62e879787756dee
Balaganesh K <k.balaganesh26@gmail.com>
2023-06-13 23:05:14 +0530

    Initial SetUp

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'primary-button': '#C9F270',
+        secondary: '#E8F0FE',
+        link: '#3365e6',
+        'primary-text': '#0e0e0e',
+        primary: '#FAFAFA',
+        'secondary-text': '#666',
+        'secondary-button': '#DAF996',
+        'text-red': '#cc3341',
+      },
+
+      transitionTimingFunction: {
+        'in-out-expo': 'cubic-bezier(0.25, 0.1, 0.25, 1)',
       },
     },
   },
   plugins: [],
-}
+};
