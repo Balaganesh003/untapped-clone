1502fb0c1affbce129df88ea8733908fa76e9c1e
Balaganesh K <k.balaganesh26@gmail.com>
2023-06-15 20:21:37 +0530

    fixed the import errors

--- pages/companies.js
+++ pages/companies.js
@@ -1,7 +1,159 @@
 import React from 'react';
+import CompanyCard from '@/components/CompanyCard';
+import JobsAndCompanies from '@/components/JobsAndCompanies';
+import CanalLogo from '@/assets/CanalLogo.png';
+import DegreedLogo from '@/assets/degreedLogo.png';
+import DiscordLogo from '@/assets/DiscordLogo.jpeg';
+import ListLogo from '@/assets/ListLogo.svg';
+import ClockLogo from '@/assets/ClockLogo.svg';
+import BookmarkLogo from '@/assets/BookmarkLogo.svg';
+import BlackTick from '@/assets/BlackTick.svg';
+import JobsCard from '@/components/JobsCard';
+
+const CompanyFilter = [
+  {
+    id: 1,
+    name: 'Location',
+  },
+  {
+    id: 2,
+    name: 'Stage',
+  },
+  {
+    id: 3,
+    name: 'Industry',
+  },
+  {
+    id: 4,
+    name: 'Company',
+  },
+  {
+    id: 5,
+    name: 'Job Title',
+  },
+  {
+    id: 6,
+    name: 'Experience Level',
+  },
+  {
+    id: 7,
+    name: 'ERGs',
+  },
+  {
+    id: 8,
+    name: 'Values',
+  },
+  {
+    id: 9,
+    name: 'Remote Culture',
+  },
+  {
+    id: 10,
+    name: 'Benefits',
+  },
+  {
+    id: 11,
+    name: 'Salary',
+  },
+];
+
+const CompanyDropdown = [
+  {
+    id: 1,
+    name: 'Recommended',
+    logo: ListLogo,
+  },
+  {
+    id: 2,
+    name: 'Newest',
+    logo: ClockLogo,
+  },
+  {
+    id: 3,
+    name: 'Applied',
+    logo: BlackTick,
+  },
+  {
+    id: 4,
+    name: 'Saved',
+    logo: BookmarkLogo,
+  },
+];
+
+const CompaniesList = [
+  {
+    id: 1,
+    role: 'Senior Software Engineer - Front End - Bengaluru',
+    company: 'Degreed',
+    location: 'Bengaluru, Ka',
+    type: 'Full Time',
+    remote: true,
+    logo: DegreedLogo,
+    no_of_applicants: 12,
+  },
+  {
+    id: 2,
+    role: 'Product Engineering Intern - Design Systems',
+    company: 'Discord',
+    location: 'San Framcosco, CA',
+    type: 'Internship',
+    remote: true,
+    logo: DiscordLogo,
+    no_of_applicants: 100,
+  },
+  {
+    id: 3,
+    role: 'Senior Frontend Software Engineer',
+    company: 'Canal',
+    location: 'San Framcosco, CA',
+    type: 'Full Time',
+    remote: true,
+    logo: CanalLogo,
+    no_of_applicants: 50,
+  },
+  {
+    id: 4,
+    role: 'Senior Software Engineer - Front End - Bengaluru',
+    company: 'Degreed',
+    location: 'Bengaluru, Ka',
+    type: 'Full Time',
+    remote: true,
+    logo: DegreedLogo,
+    no_of_applicants: 12,
+  },
+  {
+    id: 5,
+    role: 'Product Engineering Intern - Design Systems',
+    company: 'Discord',
+    location: 'San Framcosco, CA',
+    type: 'Internship',
+    remote: true,
+    logo: DiscordLogo,
+    no_of_applicants: 10,
+  },
+  {
+    id: 6,
+    role: 'Senior Frontend Software Engineer',
+    company: 'Canal',
+    location: 'San Framcosco, CA',
+    type: 'Full Time',
+    remote: true,
+    logo: CanalLogo,
+    no_of_applicants: 3,
+  },
+];
 
 const companies = () => {
-  return <div>companies</div>;
+  return (
+    <div>
+      <JobsAndCompanies
+        DropdownList={CompanyDropdown}
+        Filters={CompanyFilter}
+        CardList={CompaniesList}
+        CardName={CompanyCard}
+      />
+    </div>
+  );
 };
 
 export default companies;
